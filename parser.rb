2d34c235320309d4a8c02f82f5a3f1cc24988476
Vidar Hokstad <vidar@hokstad.com>
2009-03-08 08:16:59 -0400

    Cleanups to use the new parserbase methods and a lot of changes to get closer to Ruby

--- parser.rb
+++ parser.rb
@@ -9,43 +9,41 @@ class Parser < ParserBase
     @sexp = SEXParser.new(s)
     @shunting = OpPrec::parser(s)
   end
-
+  
   # name ::= atom
   def parse_name
-    @s.expect(Atom)
+    expect(Atom)
   end
-
+  
   # arglist ::= ("*" ws*)? name nolfws* ("," ws* arglist)?
   def parse_arglist
-    rest = false
-    if (@s.expect("*"))
-      rest = true
-      @s.ws
+    rest = expect("*")
+    ws if rest
+    if !(name = parse_name)
+      expected("argument name following '*'") if rest
+      return
     end
 
-    name = parse_name
-    raise "Expected argument name" if rest && !name
-    return nil if !name
+    @s.nolfws
+    if expect("=")
+      expect(Atom)
+      # FIXME: Store
+    end
 
     args = [(rest ? [name.to_sym,:rest] : name.to_sym)]
-
     @s.nolfws
-
-    return args if (!@s.expect(","))
-    @s.ws
-    more = parse_arglist
-    raise "Expected argument" if !more
+    expect(",") or return args
+    ws
+    more = parse_arglist or expected("argument")
     return args + more
   end
-
+  
   # args ::= nolfws* ( "(" ws* arglist ws* ")" | arglist )
   def parse_args
     @s.nolfws
-    if (@s.expect("("))
-      @s.ws
-      args = parse_arglist
-      @s.ws
-      raise "Expected ')'" if !@s.expect(")")
+    if expect("(")
+      ws; args = parse_arglist; ws
+      expect(")") or expected("')'")
       return args
     end
     return parse_arglist
@@ -56,62 +54,112 @@ class Parser < ParserBase
     @sexp.parse || @shunting.parse
   end
 
+  # if ::= "if" ws* condition defexp* "end"
+  def parse_if
+    expect("if") or return
+    ws
+    cond = parse_condition or expected("condition for 'if' block")
+    exps = zero_or_more(:defexp)
+    raise "Expected expression or 'end' for open if" if !@s.expect("end")
+    return [:if,cond,[:do]+exps]
+  end
+
   # while ::= "while" ws* condition defexp* "end"
   def parse_while
-    return nil if !@s.expect("while")
-    @s.ws
-    raise "Expected condition for 'while' block" if !(cond  = parse_condition)
+    expect("while") or return
+    ws
+    cond = parse_condition or expected("condition for 'while' block")
     exps = zero_or_more(:defexp)
-    raise "Expected 'end'" if !@s.expect("end")
+    expect("end") or expected("expression or 'end' for open 'while' block")
     return [:while,cond,[:do]+exps]
+  end
+
+  # subexp ::= exp nolfws* ("if" ws* condition)?
+  def parse_subexp
+    exp = @shunting.parse or return nil
+    @s.nolfws
+    if expect("if")
+      ws
+      cond = parse_condition or expected("condition for 'if' statement modifier")
+      exp = [:if,cond,exp]
+    end
+    return exp
   end
 
   # Later on "defexp" will allow anything other than "def"
   # and "class".
-  # defexp ::= sexp | while
+  # defexp ::= sexp | while | if | subexp
   def parse_defexp
-    @s.ws
-    @sexp.parse || parse_while || @shunting.parse
+    ws
+    ret = parse_sexp || parse_while || parse_if || parse_subexp
+    ws; expect(";"); ws
+    ret
   end
 
   # def ::= "def" ws* name args? ws* defexp* "end"
   def parse_def
-    return nil if !@s.expect("def")
-    @s.ws
-    raise "Expected function name" if !(name = parse_name)
+    expect("def") or return
+    ws
+    name = parse_name || @shunting.parse or expected("function name")
     args = parse_args || []
-    @s.ws
+    ws
     exps = zero_or_more(:defexp)
     vars = deep_collect(exps,Array) {|node| node[0] == :assign ? node[1] : nil}
     exps = [:let,vars] + exps 
-    raise "Expected expression or 'end'" if !@s.expect("end")
+    expect("end") or expected("expression or 'end' for open def")
     return [:defun, name, args, exps]
   end
 
   def parse_sexp; @sexp.parse; end
 
-  # class ::= "class" ws* name ws* exp* "end"
+  # class ::= ("class"|"module") ws* name ws* exp* "end"
   def parse_class
-    return nil if !@s.expect("class")
-    @s.ws
-    raise "Expected class name" if !(name = @s.expect(Atom))
-    @s.ws
+    type = expect("class","module") or return
+    ws
+    name = expect(Atom) or expected("class name")
+    ws
+    if expect("<")
+      ws
+      superclass = expect(Atom) or expected("superclass")
+      # FIXME: Include superclass in tree
+    end
     exps = zero_or_more(:exp)
-    raise "Expected expression or 'end'" if !@s.expect("end")
-    return [:class,name,exps]
+    expect("end") or expected("expression or 'end'")
+    return [type.to_sym,name,exps]
+  end
+
+  # require ::= "require" ws* quoted
+  def parse_require
+    expect("require") or return
+    ws
+    q = expect(Quoted) or expected("name of source to require")
+    ws
+    return [:require,q]
+  end
+
+  # include ::= "include" ws* name w
+  def parse_include
+    expect("include") or return
+    ws
+    n = parse_name or expected("name of module to include")
+    ws
+    [:include,n]
   end
 
   # exp ::= ws* (class | def | sexp)
   def parse_exp
-    @s.ws
-    parse_class || parse_def || parse_sexp || @shunting.parse
+    ws
+    ret = parse_class || parse_def || parse_defexp || parse_require || parse_include
+    ws; expect(";"); ws
+    ret
   end
 
   # program ::= exp* ws*
   def parse
     res = [:do] + zero_or_more(:exp)
-    @s.ws
+    ws
     raise "Expected EOF" if @s.peek
     return res
   end
 end
+
