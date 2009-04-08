9b8bc582311ef3225e036c57ec9cca043194e678
Vidar Hokstad <vidar@hokstad.com>
2009-04-08 03:38:25 -0400

    Start of refactoring: Deviate slightly from the shunting yard model by recursing on parantheses

--- shunting.rb
+++ shunting.rb
@@ -11,23 +11,22 @@ require 'treeoutput'
 module OpPrec
   class ShuntingYard
     def initialize output,tokenizer, parser
-      @ostack,@out,@tokenizer,@parser = [],output,tokenizer,parser
+      @out,@tokenizer,@parser = output,tokenizer,parser
     end
 
     def reset
-      @ostack = []
       @out.reset
     end
     
-    def reduce op = nil
+    def reduce ostack,op = nil
       pri = op ? op.pri : 0
       # We check for :postfix to handle cases where a postfix operator has been given a lower precedence than an
       # infix operator, yet it needs to bind tighter to tokens preceeding it than a following infix operator regardless,
       # because the alternative gives a malfored expression.
-      while  !@ostack.empty? && (@ostack[-1].pri > pri || @ostack[-1].type == :postfix)
-        o = @ostack.pop
+      while  !ostack.empty? && (ostack[-1].pri > pri || ostack[-1].type == :postfix)
+        o = ostack.pop
         @out.oper(o) if o.sym
-        return if o.type == :lp 
+        return if o.type == :lp # FIXME: Check that it is the right :lp
       end
     end
 
@@ -35,7 +34,7 @@ module OpPrec
       @parser.parse_block(start)
     end
 
-    def shunt src
+    def shunt src, ostack = []
       possible_func = false     # was the last token a possible function name?
       opstate = :prefix         # IF we get a single arity operator right now, it is a prefix operator
                                 # "opstate" is used to handle things like pre-increment and post-increment that
@@ -49,27 +48,27 @@ module OpPrec
           @out.value(nil) if op.type == :rp && lastlp # Dummy value to balance out the expressions when closing an empty pair of parentheses.
 
           if op.sym == :hash_or_block || op.sym == :block
-            if possible_func || @ostack[-1] == Operators["#call#"] || @ostack[-1] == Operators["#callm#"]
-              @out.value([]) if @ostack[-1] != Operators["#call#"]
+            if possible_func || ostack[-1] == Operators["#call#"] || ostack[-1] == Operators["#callm#"]
+              @out.value([]) if ostack[-1] != Operators["#call#"]
               @out.value(parse_block(token))
               @out.oper(Operators["#flatten#"])
-              @ostack << Operators["#call#"]  if @ostack[-1] != Operators["#call#"]
+              ostack << Operators["#call#"]  if ostack.last != Operators["#call#"]
             elsif op.sym == :hash_or_block
               op = Operators["#hash#"]
             else
               raise "Block not allowed here"
             end
           else
-            reduce(op)
+            reduce(ostack,op)
             if op.type != :rp
               opstate = :prefix
-              @ostack << (op.type == :lp && possible_func ? Operators["#call#"] : op)
+              ostack << (op.type == :lp && possible_func ? Operators["#call#"] : op)
             end
           end
         else 
           if possible_func
-            reduce
-            @ostack << Operators["#call#"]
+            reduce(ostack)
+            ostack << Operators["#call#"]
           end
           @out.value(token)
           opstate = :infix_or_postfix # After a non-operator value, any single arity operator would be either postfix,
@@ -79,19 +78,19 @@ module OpPrec
         lastlp = op && op.type == :lp
       end
 
-      if opstate == :prefix && @ostack.size && @ostack[-1] && @ostack[-1].type == :prefix
+      if opstate == :prefix && ostack.size && ostack[-1] && ostack[-1].type == :prefix
         # This is an error unless the top of the @ostack has minarity == 0,
         # which means it's ok for it to be provided with no argument
-        if @ostack[-1].minarity == 0
+        if ostack[-1].minarity == 0
           @out.value(nil)
         else
-          raise "Missing value for prefix operator #{@ostack[-1].sym.to_s}"
+          raise "Missing value for prefix operator #{ostack[-1].sym.to_s}"
         end
       end
 
-      reduce
-      return @out if  @ostack.empty?
-      raise "Syntax error. #{@ostack.inspect}"
+      reduce(ostack)
+      return @out if  ostack.empty?
+      raise "Syntax error. #{ostack.inspect}"
     end
     
     def parse
