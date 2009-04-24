ef0eb7f3aa83c2fabe23a004f3c2a8f7446c6ee1
Vidar Hokstad <vidar@hokstad.com>
2009-04-24 06:03:06 -0400

    Made ast position handling more flexible

--- ast.rb
+++ ast.rb
@@ -16,17 +16,24 @@ module AST
   #   Expr[1,2,3]
   #
   # FIXME: When called with tokens from the scanner, it is the
-  # intention that these tokens will *also* include Node
+  # intention that these tokens will *also* hav a position
   # and carry position information, and that Expr's constructor
   # will default to take the position information of the first
-  # node it is passed that is_a?(AST::Node)
+  # node it is passed that respond_to?(:position).
+  #
+  # Alternatively, if the first argument is_a?(Scanner::Position)
+  # it will be stripped and used as the position.
   class Expr < Array
     include Node
 
     def self.[] *args
+      if args.size > 0 && args.first.is_a?(Scanner::Position)
+        pos = args.shift
+      end
       e = super *args
-      sub = e.find {|n| n.is_a?(Node) }
+      sub = e.find {|n| n.respond_to?(:position) }
       e.position = sub.position if sub
+      e.position = pos if pos
       e
     end
   end
