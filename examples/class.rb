59f70aea8e264140ca8635b4d9233ebbf477ba19
Vidar Hokstad <vidar@hokstad.com>
2009-04-25 07:26:25 -0400

    Updated 'class.rb' example

--- examples/class.rb
+++ examples/class.rb
@@ -1,18 +1,4 @@
 
-def __new_class_object(size)
-  ob = malloc(size)
-  %s(assign (index ob 0) Class)
-  ob
-end
-
-class Class
-  def new
-    ob = malloc(4)
-    %s(assign (index ob 0) self)
-    ob
-  end
-end
-
 class Foo
 
   def bar
