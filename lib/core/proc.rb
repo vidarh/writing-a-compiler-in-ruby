9256f2473e51236e59e6a94b8bc7dc43419c2b1e
Vidar Hokstad <vidar@hokstad.com>
2010-01-15 16:48:09 -0500

    Improved comments

--- lib/core/proc.rb
+++ lib/core/proc.rb
@@ -1,12 +1,16 @@
 
 class Proc
+  # FIXME: Add support for handling arguments (and blocks...)
   def call
     %s(call (index self 1))
   end
 end
 
 # We can't create a Proc from a raw function directly, so we get
-# nasty:
+# nasty. The advantage of this rather ugly method is that we
+# don't in any way expose a constructor that takes raw functions
+# to normal Ruby
+#
 %s(defun __new_proc (addr)
 (let (p)
    (assign p (malloc (mul 4 (index Proc 1)))) # index 1 is the instance size
