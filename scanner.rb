0a8b33ae9c9470c7593620eccf3bdfddc334935e
root <root@aros-lenny32.(none)>
2013-05-04 23:21:47 +0000

    Handle ';' as expression terminator

--- scanner.rb
+++ scanner.rb
@@ -103,10 +103,10 @@ class Scanner
 
   # ws ::= ([\t\b\r ] | '#' [~\n]* '\n')*
   def ws
-    while (c = peek) && [9,10,13,32,?#].member?(c) do
+    while (c = peek) && [9,10,13,32,?#,?;].member?(c) do
       get
       if c == ?#
-        while (c = get) && c != "\n" do end
+        while (c = get) && c != "\n" && c != ';' do end
       end
     end
   end
