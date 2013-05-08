af5eb09763b65a0c474421d852e9a770162409da
Vidar Hokstad <vidar@hokstad.com>
2013-05-08 10:45:49 +0000

    Updated to pass w/newish rspec

--- test/function.rb
+++ test/function.rb
@@ -5,17 +5,17 @@ describe Arg do
 
   it "should have a variable amount of arguments" do
     a = Arg.new("arg1", :rest)
-    a.rest?.should == true
+    a.rest?.should be true
   end
 
   it "should be of type :argaddr" do
     a = Arg.new("arg2", :rest)
-    a.type.should == :argaddr
+    a.type.should be :argaddr
   end
 
   it "should be of type :arg" do
     a = Arg.new("arg3")
-    a.type.should == :arg
+    a.type.should be :arg
   end
 
 end
@@ -29,35 +29,24 @@ describe Function do
     f = Function.new(args, body, nil)
   end
 
-  it "should not be a method" do
-    f = Function.new("not_a_method", [])
-    f.is_method?.should == false
-  end
-
-  it "should be a method" do
-    cscope = ClassScope.new(nil, "cscope", nil)
-    f = Function.new([:some_arg], [], cscope)
-    f.is_method?.should == true
-  end
-
   it "should return the correct number of arguments" do
     f = function_with_args
-    f.get_arg(:numargs).should == [:int, 3]
+    f.get_arg(:numargs).should match_array([:int, 3])
   end
 
   it "should find an arg within argument list" do
     f = function_with_args
-    f.get_arg(:arg1).should == [:arg, 0]
+    f.get_arg(:arg1).should match_array([:arg, 0])
   end
 
   it "should not have a variable argument list" do
     f = function_with_args
-    f.rest?.should == false
+    f.rest?.should be false
   end
 
   it "should have a variable argument list" do
     f = Function.new([:arg1, [:args, :rest]], [], nil)
-    f.rest?.should == true
+    f.rest?.should be true
   end
 
 end
