df6ae1a35a5d3c5290c544a1a43608d53d0ba846
Vidar Hokstad <vidar@hokstad.com>
2013-05-11 00:43:50 +0000

    Add missing test for #clean_method_name

---
+++ test/compiler.rb
@@ -0,0 +1,15 @@
+
+require 'compiler'
+
+describe Compiler do
+
+  describe "#clean_method_name" do
+
+    it "should escape all characters outside [a-zA-Z0-9_]" do
+      input = (0..255).collect.pack("c*")
+      output = Compiler.new.clean_method_name(input)
+      expect(output.match("([0-9a-zA-Z_])+")[0]).to eq(output)
+    end
+
+  end
+end
