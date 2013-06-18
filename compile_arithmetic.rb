a23f0d00d0c6b12dd39eee309d8bbcd87455cd58
Vidar Hokstad <vidar@hokstad.com>
2013-06-18 01:20:33 +0000

    Fix evaluation order for sub, and refactor add/sub to use a shared helper

--- compile_arithmetic.rb
+++ compile_arithmetic.rb
@@ -2,24 +2,27 @@
 # Method to compile arithmetic
 class Compiler
 
-  def compile_add(scope, left, right)
+  def compile_2(scope, left, right)
     @e.with_register do |reg|
       src = compile_eval_arg(scope,left)
       @e.movl(src,reg)
       @e.save_result(compile_eval_arg(scope,right))
+      yield reg
+    end
+    [:subexpr]    
+  end
+
+  def compile_add(scope, left, right)
+    compile_2(scope,left,right) do |reg|
       @e.addl(reg, :eax)
     end
-    [:subexpr]
   end
 
   def compile_sub(scope, left, right)
-    @e.with_register do |reg|
-      src = compile_eval_arg(scope,left)
-      @e.movl(src,reg)
-      @e.save_result(compile_eval_arg(scope,right))
-      @e.subl(reg, :eax)
+    compile_2(scope,left,right) do |reg|
+      @e.subl(:eax,reg)
+      @e.save_result(reg)
     end
-    [:subexpr]
   end
 
 end
