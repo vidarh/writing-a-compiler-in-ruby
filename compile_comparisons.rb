bba2c93aff5a0c16e46f98734388772e2ef37d79
Vidar Hokstad <vidar@hokstad.com>
2013-06-27 04:01:51 +0000

    Compile comparison operators

---
+++ compile_comparisons.rb
@@ -0,0 +1,38 @@
+
+class Compiler
+
+  # compile_2 was added in compile_arithmetic.rb
+  # FIXME: Split these helpers out in separate file
+  def compile_comparison(scope, op, left, right)
+    compile_2(scope,left,right) do |reg|
+      @e.cmpl(:eax,reg)
+      @e.emit("set#{op.to_s}".to_sym, :al)
+      @e.movzbl(:al,:eax)
+    end
+  end
+
+  def compile_eq(scope,left,right)
+    compile_comparison(scope, :e, left,right)
+  end
+
+  def compile_ne(scope,left,right)
+    compile_comparison(scope, :ne, left,right)
+  end
+
+  def compile_lt(scope,left,right)
+    compile_comparison(scope, :l, left,right)
+  end
+
+  def compile_le(scope,left,right)
+    compile_comparison(scope, :le, left,right)
+  end
+
+  def compile_gt(scope,left,right)
+    compile_comparison(scope, :g, left,right)
+  end
+
+  def compile_ge(scope,left,right)
+    compile_comparison(scope, :ge, left,right)
+  end
+
+end
