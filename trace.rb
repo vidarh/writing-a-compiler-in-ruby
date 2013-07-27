b31e22ed8b1b0b73d6d39fb88bb37d860dd0f19b
Vidar Hokstad <vidar@hokstad.com>
2013-07-27 23:55:35 +0000

    Add support for trace output

---
+++ trace.rb
@@ -0,0 +1,35 @@
+
+class Compiler
+
+  # Generating trace output for debugging purposes
+
+  def trace(pos,data)
+    return false if !@trace
+    
+    # A bit ugly, but prevents infinite recursion
+    # if we accidentally calls anything "traceworthy":
+    @trace = false 
+
+    @e.with_stack(2,true) do
+        if pos
+          pos = pos.short
+        end
+        sio = StringIO.new
+        sio << "#{pos ? pos+":" : "  "} "
+
+        if data.is_a?(String)
+          sio << data << "\n"
+        else
+          print_sexp(data,sio,{:prune => true})
+        end
+        str = sio.string
+        ret = compile_eval_arg(nil,str)
+        @e.save_to_stack(ret,0)
+        @e.movl([:stderr],:eax)
+        @e.save_to_stack(:eax,1)
+        @e.call("fputs") #compile_eval_arg(scope,:puts))
+      end
+      @trace = true
+  end
+
+end
