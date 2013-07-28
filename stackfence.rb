dde1814905c290909f8fb99600f888a7856e655d
Vidar Hokstad <vidar@hokstad.com>
2013-07-28 01:15:04 +0000

    Add stack fence code

---
+++ stackfence.rb
@@ -0,0 +1,34 @@
+
+class Compiler
+
+  # If @stackfence is set, calling this method with a block
+  # will result in a pseudo-random value getting pushed onto
+  # the stack, and on return from the block, that value is
+  # compared against the top of the stack. If a match if found,
+  # all is well and the value is removed from the stack.
+  #
+  # Otherwise `trace` is called (FIXME: and a way of forcing trace
+  # output needs to be added) with an error message. 
+  #
+  # This provides some validation both that the stack is not
+  # pushed/popped unevenly, and that values are not overwritten.
+  #
+  def stackfence
+    if !@stackfence
+      return yield
+    end
+
+    # Note: We don't care if this value is all that random.
+    val = (rand * (2**32)).floor
+    @e.pushl(val)
+    r = yield
+    @e.cmpl("$#{val}","(%esp)")
+    l = @e.get_local
+    @e.jz(l)
+    trace(nil,"ERROR: Stack fence violation. Expected 0x#{val.to_s(16)}")
+    @e.local(l)
+    @e.addl(4,:esp)
+    r
+  end
+
+end
