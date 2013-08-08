066b8e6b918a203400bc1858187afca6e10e5db0
Vidar Hokstad <vidar@hokstad.com>
2013-08-08 01:53:14 +0000

    Add printregs utility/debug function for use with saveregs

--- lib/core/core.rb
+++ lib/core/core.rb
@@ -66,3 +66,16 @@ false = 0    # FIXME: Should be an object of FalseClass
 %s(defun range (a b)
   (puts "Compiler range construct is not implemented yet")
 )
+
+
+%s(defun printregs (regs) (do
+  (printf "eax: %08x, ebx: %08x, ecx: %08x, edx: %08x, esi: %08x, edi: %08x, ebp: %08x, esp: %08x\n"
+   (index regs 0)
+   (index regs 1)
+   (index regs 2)
+   (index regs 3)
+   (index regs 4)
+   (index regs 5)
+   (index regs 6)
+   (index regs 7))
+))
