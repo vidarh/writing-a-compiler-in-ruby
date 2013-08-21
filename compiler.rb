4201eb0da3038c1e98dd6511516e936f8060b92a
Vidar Hokstad <vidar@hokstad.com>
2013-08-21 15:06:14 +0000

    Split splat handling out into its own file

--- compiler.rb
+++ compiler.rb
@@ -16,6 +16,7 @@ require 'compile_comparisons'
 require 'trace'
 require 'stackfence'
 require 'saveregs'
+require 'splat'
 
 class Compiler
   attr_reader :global_functions
@@ -439,65 +440,6 @@ class Compiler
     end
     @e.comment("yield end")
     return [:subexpr]
-  end
-
-  def handle_splat(scope,args)
-    # FIXME: Quick and dirty splat handling:
-    # - If the last node has a splat, we cheat and assume it's
-    #   from the arguments rather than a proper Ruby Array.
-    # - We assume we can just allocate args.length+1+numargs
-    # - We wastefully do it in two rounds and muck directly
-    #   with %esp for now until I figure out how to do this
-    #   more cleanly.
-    splat = args.last.is_a?(Array) && args.last.first == :splat
-    numargs = nil
-
-    if !splat
-      return yield(args)
-    end
-
-    # FIXME: This is just a disaster waiting to happen
-    # (needs proper register allocation)
-    @e.comment("*#{args.last.last.to_s}")
-    reg = compile_eval_arg(scope,:numargs)
-    @e.subl(args.size-1,reg)
-    @e.sall(2,reg)
-    @e.subl(reg,@e.sp)
-    
-    @e.with_register do |argend|
-      @e.movl(reg,argend) 
-      
-      reg = compile_eval_arg(scope,args.last.last)
-      @e.addl(reg,argend)
-      
-      @e.with_register do |dest|
-        @e.movl(@e.sp,dest)
-        l = @e.local
-        @e.load_indirect(reg,@e.scratch)
-        @e.save_indirect(@e.scratch,dest)
-        @e.addl(4,@e.result)
-        @e.addl(4,dest)
-        @e.cmpl(reg,argend)
-        @e.jne(l)
-        @e.subl(@e.sp,dest)
-        @e.sarl(2,dest)
-        @e.subl(1,dest)
-        @e.addl(dest,@e.scratch)
-        @e.comment("*#{args.last.last.to_s} end")
-        
-        args.pop
-      end
-    end
-
-    yield(args)
-
-    @e.pushl(@e.result)
-    reg = compile_eval_arg(scope,:numargs)
-    @e.subl(args.size,reg)
-    @e.sall(2,reg)
-    @e.movl(reg,@e.scratch)
-    @e.popl(@e.result)
-    @e.addl(@e.scratch,@e.sp)
   end
 
   def compile_callm_args(scope, ob, args)
---
+++ splat.rb
@@ -0,0 +1,63 @@
+
+class Compiler
+
+  def handle_splat(scope,args)
+    # FIXME: Quick and dirty splat handling:
+    # - If the last node has a splat, we cheat and assume it's
+    #   from the arguments rather than a proper Ruby Array.
+    # - We assume we can just allocate args.length+1+numargs
+    # - We wastefully do it in two rounds and muck directly
+    #   with %esp for now until I figure out how to do this
+    #   more cleanly.
+    splat = args.last.is_a?(Array) && args.last.first == :splat
+    numargs = nil
+
+    if !splat
+      return yield(args)
+    end
+
+    # FIXME: This is just a disaster waiting to happen
+    # (needs proper register allocation)
+    @e.comment("*#{args.last.last.to_s}")
+    reg = compile_eval_arg(scope,:numargs)
+    @e.subl(args.size-1,reg)
+    @e.sall(2,reg)
+    @e.subl(reg,@e.sp)
+    
+    @e.with_register do |argend|
+      @e.movl(reg,argend) 
+      
+      reg = compile_eval_arg(scope,args.last.last)
+      @e.addl(reg,argend)
+      
+      @e.with_register do |dest|
+        @e.movl(@e.sp,dest)
+        l = @e.local
+        @e.load_indirect(reg,@e.scratch)
+        @e.save_indirect(@e.scratch,dest)
+        @e.addl(4,@e.result)
+        @e.addl(4,dest)
+        @e.cmpl(reg,argend)
+        @e.jne(l)
+        @e.subl(@e.sp,dest)
+        @e.sarl(2,dest)
+        @e.subl(1,dest)
+        @e.addl(dest,@e.scratch)
+        @e.comment("*#{args.last.last.to_s} end")
+        
+        args.pop
+      end
+    end
+
+    yield(args)
+
+    @e.pushl(@e.result)
+    reg = compile_eval_arg(scope,:numargs)
+    @e.subl(args.size,reg)
+    @e.sall(2,reg)
+    @e.movl(reg,@e.scratch)
+    @e.popl(@e.result)
+    @e.addl(@e.scratch,@e.sp)
+  end
+
+end
