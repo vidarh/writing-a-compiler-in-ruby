cf96bd2d98c9cdc831016cfaefd32948104a90ae
Vidar Hokstad <vidar@hokstad.com>
2014-03-25 20:53:48 +0000

    Ruby 1.9 compatibility for test

--- test/compiler.rb
+++ test/compiler.rb
@@ -1,4 +1,5 @@
 
+$: << File.dirname(__FILE__)+"/.."
 require 'compiler'
 
 describe Compiler do
@@ -6,7 +7,7 @@ describe Compiler do
   describe "#clean_method_name" do
 
     it "should escape all characters outside [a-zA-Z0-9_]" do
-      input = (0..255).collect.pack("c*")
+      input = (0..255).to_a.pack("c*")
       output = Compiler.new.clean_method_name(input)
       expect(output.match("([0-9a-zA-Z_])+")[0]).to eq(output)
     end
