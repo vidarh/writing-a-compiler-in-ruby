3a7e27500f2dcc33942ec8373a933770f47da785
Vidar Hokstad <vidar@hokstad.com>
2014-09-21 03:44:30 +0000

    Add default implementations for new Scope#add_constant and Scope#find_constant

--- scope.rb
+++ scope.rb
@@ -4,6 +4,14 @@ class Scope
   def method
     nil
   end
+
+  def add_constant(c)
+    @next.add_constant(c) if @next
+  end
+
+  def find_constant(c)
+    @next.find_constant(c) if @next
+  end
 end
 
 
