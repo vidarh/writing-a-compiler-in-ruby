a975c68e29a7ddea4bce9b9023a17a6ffeb76580
Vidar Hokstad <vidar@hokstad.com>
2015-06-04 00:46:12 +0000

    Tests for Symbol identity

---
+++ features/inputs/symbol_identity.rb
@@ -0,0 +1,24 @@
+
+a = :foo
+b = :bar
+c = :foo
+
+aid = a.object_id
+bid = b.object_id
+cid = c.object_id
+
+puts a.to_s
+puts b.to_s
+puts c.to_s
+
+if aid == cid
+  puts "OK:   aid == cid"
+else
+  puts "FAIL: aid != cid"
+end
+
+if aid != bid
+  puts "OK:   aid != bid"
+else
+  puts "FAIL: aid == bid"
+end
