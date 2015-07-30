23316f22eb5c033aa44059be0e0c807b8ee7bfaa
Vidar Hokstad <vidar@hokstad.com>
2015-07-30 04:19:22 +0000

    Add quick and dirty partial impl. of Set

--- lib/set.rb
+++ lib/set.rb
@@ -1,3 +1,27 @@
 
+#
+# FIXME
+# This is an awful, quick and dirty stub of a Set implementation
+# to get some of the basics in place
+#
 class Set
+  def initialize
+    @set = Hash.new # Told you it was dirty
+  end
+
+  def << k
+    @set[k]=1
+  end
+
+  def to_a
+    @set.keys
+  end
+
+  def self.[] *args
+    s = Set.new
+    args.each do |a|
+      s << a
+    end
+    s
+  end
 end
