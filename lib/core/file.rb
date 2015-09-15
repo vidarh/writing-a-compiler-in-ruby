87fd711222f705d8bfbc970d04313e5636491ca7
Vidar Hokstad <vidar@hokstad.com>
2015-09-15 01:31:34 +0000

    Stub out (non-functioning) versions of File.dirname and File.expand_path

--- lib/core/file.rb
+++ lib/core/file.rb
@@ -4,4 +4,14 @@ class File
   def self.file?(io)
     false
   end
+
+  def self.dirname(dname)
+    STDERR.puts "dirname: #{dname}"
+    "XXX"
+  end
+
+  def self.expand_path(path)
+    STDERR.puts "expand_path: #{path}"
+    "YYY"
+  end
 end
