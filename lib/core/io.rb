fb3a67a92e431259cd8d040bd58047ab61f24341
Vidar Hokstad <vidar@hokstad.com>
2016-08-29 20:40:35 +0100

    Cleaned up and improved efficiency of IO class somewhat but pre-allocating a buffer

--- lib/core/io.rb
+++ lib/core/io.rb
@@ -1,23 +1,25 @@
 
+# FIXME: This code is quite inefficient because we don't
+# have an easy way of getting the address of a stack allocated
+# variable (yet). Oh, and it leaks memory since we don't
+# GC yet.
+
+
 class IO < Object
-  def initialize
-#    %s(puts "IO.new")
+  def initialize fd
+    @fd = fd
+
+    %s(assign tmp (malloc 256))
+    @rawbuf = tmp
   end
 
   def getc
     c = 0
     tmp = 0
 
-    # FIXME: This code is specific to a 32 bit little endian
-    # arch, and is also horribly inefficient because we don't
-    # have an easy way of getting the address of a stack allocated
-    # variable.
     %s(do
-         (assign tmp (malloc 4))
-         (assign (index tmp 0) 0)
-         (read 0 tmp 1)
-         (assign c (__get_fixnum (index tmp 0)))
-         (free tmp)
+         (read (callm @fd __get_raw) @rawbuf 1)
+         (assign c (__get_fixnum (bindex @rawbuf 0)))
          )
     c
   end
