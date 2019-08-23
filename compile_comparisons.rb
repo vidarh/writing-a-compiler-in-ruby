09e5e93c5ebbb69f1fcdf570b0d3e05f6f6e1983
Vidar Hokstad <vidar@hokstad.com>
2019-08-23 04:42:53 +0100

    Workaround for compiler bug

--- compile_comparisons.rb
+++ compile_comparisons.rb
@@ -4,9 +4,14 @@ class Compiler
   # compile_2 was added in compile_arithmetic.rb
   # FIXME: Split these helpers out in separate file
   def compile_comparison(scope, op, left, right)
+    o = op
     compile_2(scope,left,right) do |reg|
+      # FIXME @bug set#{op.to_s}.to_sym
+      # below ends up picking up reg instead of op.
       @e.cmpl(@e.result,reg)
-      @e.emit("set#{op.to_s}".to_sym, :al)
+      #@e.emit("set#{op.to_s}".to_sym, :al)
+      ox = "set#{o.to_s}"
+      @e.emit(ox.to_sym, :al)
       @e.movzbl(:al,@e.result)
     end
   end
