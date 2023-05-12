d948143cf2edcc08cd57bbe53b07e47c0257b244
Vidar Hokstad <vidar@hokstad.com>
2023-05-12 17:52:53 +0100

    Cleanups and improve clarity, combined with reducing unnecessary object creation by "caching" commonly used string literals.

--- parser.rb
+++ parser.rb
@@ -36,9 +36,18 @@ class Parser < ParserBase
     expect(Atom)
   end
 
+  ASTERISK="*"
+  AMP="&"
+  LP="("
+  RP=")"
+  COMMA=","
+  COLON=":"
+  SEMICOLON=";"
+  PIPE="|"
+
   # arglist ::= ("*" ws*)? name nolfws* ("," ws* arglist)?
   def parse_arglist
-    prefix = expect("*") || expect("&")
+    prefix = literal(ASTERISK) || literal(AMP)
     ws if prefix
     ## FIXME: If "name" is not mentioned here, it is not correctly recognised
     name = nil
@@ -49,19 +58,19 @@ class Parser < ParserBase
 
     nolfws
     default = nil
-    if expect("=")
+    if literal("=")
       nolfws
-      default = @shunting.parse([","])
+      default = @shunting.parse([COMMA])
     end
 
-    if prefix then args = [[name.to_sym, prefix == "*" ? :rest : :block]]
+    if prefix then args = [[name.to_sym, prefix == ASTERISK ? :rest : :block]]
     elsif default
       args = [[name.to_sym, :default, default]]
-    else 
+    else
       args = [name.to_sym]
     end
     nolfws
-    expect(",") or return args
+    literal(COMMA) or return args
     ws
     more = parse_arglist or expected("argument")
     return args + more
@@ -70,9 +79,9 @@ class Parser < ParserBase
   # args ::= nolfws* ( "(" ws* arglist ws* ")" | arglist )
   def parse_args
     nolfws
-    if expect("(")
+    if literal(LP)
       ws; args = parse_arglist; ws
-      expect(")") or expected("')'")
+      literal(RP) or expected("')'")
       return args
     end
     return parse_arglist
@@ -94,7 +103,7 @@ class Parser < ParserBase
     type = keyword(:if) || keyword(:unless) or return
     parse_if_body(type.to_sym)
   end
-  
+
   # FIXME: Weird parser bug: If '"then' appears together in the comment
   # line before, it causes a parse failure
   # if_body ::= ws* condition nolfws* ";"? nolfws* "then"? ws* 
@@ -103,22 +112,22 @@ class Parser < ParserBase
     pos = position
     ws
     cond = parse_condition or expected("condition for '#{type.to_s}' block")
-    nolfws; expect(";")
-    nolfws; expect(:then); ws;
-    exps = zero_or_more(:defexp)
+    nolfws; literal(";")
+    nolfws; keyword(:then); ws;
+    exps = parse_opt_defexp
     ws
 
     # FIXME: Workaround for intialization error
     elseexps = nil
-    if expect(:elsif)
+    if keyword(:elsif)
       # We treat "if ... elif ... else ... end" as shorthand for "if ... else if ... else ... end; end"
       elseexps = [parse_if_body(:if)]
     else
-      if expect(:else)
+      if keyword(:else)
         ws
-        elseexps = zero_or_more(:defexp)
+        elseexps = parse_opt_defexp
       end
-      expect(:end) or expected("expression or 'end' for open 'if'")
+      keyword(:end) or expected("expression or 'end' for open 'if'")
     end
     ret = E[pos,type.to_sym, cond, E[:do].concat(exps)]
     ret << E[:do].concat(elseexps) if elseexps
@@ -128,31 +137,30 @@ class Parser < ParserBase
   # when ::= "when" ws* condition (nolfws* ":")? ws* defexp*
   def parse_when
     pos = position
-    expect(:when) or return
+    keyword(:when) or return
     ws
     cond = parse_condition or expected("condition for 'when'")
     nolfws
-    expect(":")
+    literal(COLON)
     ws
-    exps = zero_or_more(:defexp)
-    return E[:when, cond, exps]
+    return E[:when, cond, parse_opt_defexp]
   end
 
   # case ::= "case" ws* condition when* ("else" ws* defexp*) "end"
   def parse_case
     pos = position
-    expect(:case) or return
+    keyword(:case) or return
     ws
     cond = parse_condition or expected("condition for 'case' block")
     ws
-    whens = zero_or_more(:when)
+    whens = kleene { parse_when }
     ws
-    if expect(:else)
+    if keyword(:else)
       ws
-      elses = zero_or_more(:defexp)
+      elses = parse_opt_defexp
     end
     ws
-    expect(:end) or expected("'end' for open 'case'")
+    keyword(:end) or expected("'end' for open 'case'")
     return E[pos, :case, cond, whens, elses].compact
   end
 
@@ -160,41 +168,40 @@ class Parser < ParserBase
   # while ::= "while" ws* condition "do"? defexp* "end"
   def parse_while
     pos = position
-    expect(:while) or return
+    keyword(:while) or return
     ws
     cond = parse_condition or expected("condition for 'while' block")
-    nolfws; expect(";"); nolfws; expect(:do)
+    nolfws; literal(SEMICOLON); nolfws; keyword(:do)
     nolfws;
-    exps = zero_or_more(:defexp)
-    expect(:end) or expected("expression or 'end' for open 'while' block")
+    exps = parse_opt_defexp
+    keyword(:end) or expected("expression or 'end' for open 'while' block")
     return E[pos, :while, cond, [:do]+exps]
   end
 
   # rescue ::= "rescue" (nolfws* name nolfws* ("=>" ws* name)?)? ws defexp*
   def parse_rescue
     pos = position
-    expect(:rescue) or return
+    keyword(:rescue) or return
     nolfws
     if c = parse_name
       nolfws
-      if expect("=>")
+      if literal("=>")
         ws
         name = parse_name or expected("variable to hold exception")
       end
     end
     ws
-    exps = zero_or_more(:defexp)
-    return E[pos, :rescue, c, name, exps]
+    return E[pos, :rescue, c, name, parse_opt_defexp]
   end
 
   # begin ::= "begin" ws* defexp* rescue? "end"
   def parse_begin
     pos = position
-    expect(:begin) or return
+    keyword(:begin) or return
     ws
-    exps = zero_or_more(:defexp)
+    exps = parse_opt_defexp
     rescue_ = parse_rescue
-    expect(:end) or expected("expression or 'end' for open 'begin' block")
+    keyword(:end) or expected("expression or 'end' for open 'begin' block")
     return E[pos, :block, [], exps, rescue_]
   end
 
@@ -212,7 +219,7 @@ class Parser < ParserBase
   # lambda ::= "lambda" *ws block
   def parse_lambda
     pos = position
-    expect(:lambda) or return
+    keyword(:lambda) or return
     ws
     block = parse_block or expected("do .. end block")
     return E[pos, :lambda, *block[1..-1]]
@@ -220,13 +227,13 @@ class Parser < ParserBase
 
   def parse_break
     pos = position
-    return nil if !expect(:break)
+    return nil if !keyword(:break)
     return E[pos, :break]
   end
 
   def parse_next
     pos = position
-    return nil if !expect(:next)
+    return nil if !keyword(:next)
     exps = parse_subexp
     # FIXME: Compiler @bug workaround:
     # Current splat handling crashes if argument is not
@@ -256,19 +263,30 @@ class Parser < ParserBase
       # from "normal" :if/:while
       ws
       cond = parse_condition or expected("condition for '#{sym.to_s}' statement modifier")
-      nolfws; expect(";")
+      nolfws; literal(SEMICOLON)
       ret = E[pos, sym.to_sym, cond, ret]
     end
-    #ws; expect(";"); ws
+    #ws; literal(";"); ws
     return ret
+  end
+
+  def parse_opt_defexp
+    exps = E[position]
+    while ret = parse_defexp
+      exps << ret
+    end
+    exps
   end
 
   # block_body ::=  ws * defexp*
   def parse_block_exps
     pos = position
     ws
-    exps = zero_or_more(:defexp)
-    return E[pos,*exps]
+    exps = E[pos]
+    while ret = parse_defexp
+      exps << ret
+    end
+    return exps
   end
 
   def parse_block(start = nil)
@@ -277,7 +295,7 @@ class Parser < ParserBase
     close = (start.to_s == "{") ? "}" : :end
     ws
     args = []
-    if expect("|")
+    if literal(PIPE)
       ws
       # FIXME:
       # This is a workaround, as
@@ -286,15 +304,15 @@ class Parser < ParserBase
       while name = parse_name
         args << name
         ws
-        break if !expect(",")
+        break if !literal(COMMA)
         ws
       end
       ws
-      expect("|")
+      literal(PIPE)
     end
     exps = parse_block_exps
     ws
-    expect(close) or expected("'#{close.to_s}' for '#{start.to_s}'-block")
+    literal(close) or expected("'#{close.to_s}' for '#{start.to_s}'-block")
     return E[pos, :proc ] if args.size == 0 and exps.size == 0
     return E[pos, :proc, args, exps]
   end
@@ -315,13 +333,13 @@ class Parser < ParserBase
   # def ::= "def" ws* name args? block_body
   def parse_def
     pos = position
-    expect(:def) or return nil
+    keyword(:def) or return nil
     ws
     name = parse_defname
     args = parse_args || []
-    expect(";")
+    literal(";")
     exps = parse_block_exps
-    expect(:end) or expected("expression or 'end' for open def '#{name.to_s}'")
+    keyword(:end) or expected("expression or 'end' for open def '#{name.to_s}'")
     return E[pos, :defm, name, args, exps]
   end
 
@@ -330,22 +348,22 @@ class Parser < ParserBase
   # module ::= "module" ws* name ws* exp* "end"
   def parse_module
     pos = position
-    type = expect(:module) or return
+    type = keyword(:module) or return
     ws
-    name = expect(Atom) || expect('<<') or expected("class name")
+    name = expect(Atom) || literal('<<') or expected("class name")
     ws
     error("A module can not have a super class") if @scanner.peek == ?<
-    exps = zero_or_more(:exp)
-    expect(:end) or expected("expression or \'end\'")
+    exps = kleene { parse_exp }
+    keyword(:end) or expected("expression or \'end\'")
     return E[pos, type.to_sym, name, :Object, exps]
   end
 
   # class ::= ("class" ws* (name|'<<') ws* (< ws* superclass)? ws* name ws* exp* "end"
   def parse_class
     pos = position
-    type = expect(:class) or return
+    type = keyword(:class) or return
     ws
-    name = expect(Atom) || expect("\<\<") or expected("class name")
+    name = expect(Atom) || literal("\<\<") or expected("class name")
     ws
     # FIXME: Workaround for initialization error
     superclass = nil
@@ -353,8 +371,8 @@ class Parser < ParserBase
       ws
       superclass = expect(Atom) or expected("superclass")
     end
-    exps = zero_or_more(:exp)
-    expect(:end) or expected("expression or 'end'")
+    exps = kleene { parse_exp }
+    keyword(:end) or expected("expression or 'end'")
     return E[pos, type.to_sym, name, superclass || :Object, exps]
   end
 
@@ -424,7 +442,7 @@ class Parser < ParserBase
   # require ::= "require" ws* subexp
   def parse_require
     pos = position
-    expect(:require) or return
+    keyword(:require) or return
     ws
     q = parse_subexp or expected("name of source to require")
     ws
@@ -451,13 +469,13 @@ class Parser < ParserBase
   def parse_exp
     ws
     pos = position
-    ret = parse_class || parse_module || parse_def || parse_include || parse_defexp || expect("protected")
+    ret = parse_class || parse_module || parse_def || parse_include || parse_defexp || literal("protected")
     if ret.is_a?(Array)
       ret = E[pos].concat(ret)
     elsif ret.respond_to?(:position) && !ret.position
       ret.position = pos
     end
-    ws; expect(";"); ws
+    ws; expect(SEMICOLON); ws
     return ret
   end
 
@@ -469,10 +487,9 @@ class Parser < ParserBase
     # runtime issue.
     # res << self.require(File.expand_path(File.dirname(__FILE__)+"/lib/core/core.rb")) if require_core and !@opts[:norequire]
     res << self.require("core/core.rb") if require_core and !@opts[:norequire]
-    res.concat(zero_or_more(:exp))
+    res.concat(kleene { parse_exp })
     ws
     error("Expected EOF") if scanner.peek
     return res
   end
 end
-
