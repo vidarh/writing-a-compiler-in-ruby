3865f8f21d8c6aa1287df914020590f09dbf361f
Vidar Hokstad <vidar@hokstad.com>
2025-10-08 00:18:27 +0100

    Fix selftest-c by removing Integer() global method
    The Integer() method defined at top-level in lib/core/integer.rb
    breaks self-hosting compilation (selftest-c). The compiler has
    issues with top-level method definitions that aren't in Object.
    
    This reverts part of commit d72a0d8 which added Integer().
    
    Impact: ceil/floor/ceildiv specs that depended on Integer() will
    crash again, but selftest-c now passes.
    
    make selftest: PASS
    make selftest-c: PASS

--- lib/core/integer.rb
+++ lib/core/integer.rb
@@ -14,15 +14,3 @@ class Integer < Numeric
   end
 
 end
-
-# FIXME: Belongs in Kernel
-# FIXME: Stub - should handle base parameter and string conversion properly
-def Integer(arg, base = 10)
-  if arg.respond_to?(:to_int)
-    arg.to_int
-  elsif arg.respond_to?(:to_i)
-    arg.to_i
-  else
-    0
-  end
-end
