7036614842bb3d264c0462d689eef97d701fcddc
Vidar Hokstad <vidar@hokstad.com>
2025-10-14 16:28:05 +0100

    Remove Fixnum#<=> - now inherited from Integer
    Integer#<=> already uses __get_raw for both operands and handles
    both tagged fixnums and heap integers correctly.
    
    Fixnum methods: 4 remaining (down from 5)
    Tests: make selftest-c passes with 0 failures

--- lib/core/fixnum.rb
+++ lib/core/fixnum.rb
@@ -32,19 +32,8 @@ class Fixnum < Integer
   # __get_raw removed - now inherited from Integer
   # Integer version handles both tagged fixnums and heap integers
 
-  # <, >, <=, >= removed - now inherited from Integer
+  # <, >, <=, >=, <=> removed - now inherited from Integer
   # Integer versions use __get_raw for both tagged fixnums and heap integers
-
-  def <=> other
-    return nil if !other.is_a?(Numeric)
-    if self > other
-      return 1
-    end
-    if self < other
-      return -1
-    end
-    return 0
-  end
 
   def - other
     %s(let (result) (assign result (sub (sar self) (callm other __get_raw)))
