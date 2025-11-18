603ac799692cb1e1f83bea34d12c7af149096e53
Vidar Hokstad <vidar@hokstad.com>
2025-11-18 08:05:41 +0000

    Stub

---
+++ lib/core/rbconfig.rb
@@ -0,0 +1,8 @@
+# Stub implementation of RbConfig for compiler compatibility
+module RbConfig
+  CONFIG = {
+    "EXTSTATIC" => "static",
+    "ruby_version" => "2.5.0",
+    "host_os" => "linux"
+  }
+end
